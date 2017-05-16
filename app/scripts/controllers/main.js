553466c54c5e40f463c4380b3ba3fba2cb741ea3
Master Two T <cresson111@gmail.com>
2017-05-16 23:26:34 +0200

    update controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,64 +9,51 @@
  */
 angular.module('jeyniFrontApp')
   .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
-    var deleteStyle = function (element) {
-        var childElements = element.childNodes;
-        var i;
-        for (i = 0; i < childElements.length; i++) {
-            if (childElements[i].nodeName === 'STYLE') {
-                childElements[i].parentNode.removeChild(childElements[i]);
-            }
-        }
-    };
-    var carGetRequest = {
-        'id': '',
-        'name': '',
-        'ownerName': '',
-        'seatNumber': ''
-    };
-    $scope.carPostRequest = {
-        'id': '',
-        'name': '',
-        'ownerName': '',
-        'seatNumber': ''
-    };
-    $scope.submitRequest = function () {
-        console.log('submitRequest begin');
-        var dataToSend = angular.toJson($scope.carPostRequest, true);
-        $http({
-            method: 'POST',
-            url: 'http://localhost:9090/jeyni-service/rest/car/create',
-             headers: {
-                'Content-Type': 'application/json'
-            },
-            data: dataToSend
-        }).then(function successCallback(success) {
-            console.log('submitRequest success: ',success);
-        }, function errorCallback(error) {
-            console.log('submitRequest error: ',error);
-            var errorElements = document.getElementsByClassName('error');
-            errorElements[0].innerHTML = error.data;
-            deleteStyle(errorElements[0]);
-            errorElements[0].style.borderColor = 'black';
-            errorElements[0].style.borderStyle = 'solid';
-            errorElements[0].style.borderWidth = '1px';
-        }).finally(function(){
-            console.log('submitRequest end');
-        });
-    };
-    $scope.getRequest = function () {
-        console.log('getRequest begin');
-        $http({
-            method: 'GET',
-            url: 'http://localhost:9090/jeyni-service/rest/car/get'
-        }).then(function successCallback(success) {
-            console.log('getRequest success: ',success);
-            carGetRequest = success.data;
-            $scope.carGetResquest = carGetRequest;
-        }, function errorCallback(error) {
-            console.log('getRequest error: ',error);
-        }).finally(function(){
-            console.log('getRequest end');
-        });
-    };
-  }]);
+  var serviceUrl = 'http://localhost:9090/jeyni-service/rest/';
+  var carGetRequest = {
+    'name': '',
+    'ownerName': '',
+    'seatNumber': ''
+  };
+  $scope.carPostRequest = {
+    'name': '',
+    'ownerName': '',
+    'seatNumber': ''
+  };
+  $scope.submitRequest = function () {
+    console.log('submitRequest begin');
+    var dataToSend = angular.toJson($scope.carPostRequest, true);
+    $http({
+      method: 'POST',
+      url: serviceUrl + 'car/create',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      data: dataToSend
+    }).then(function successCallback(success) {
+      console.log('submitRequest success: ', success);
+    }, function errorCallback(error) {
+      console.log('submitRequest error: ', error);
+    }).finally(function () {
+      console.log('submitRequest end');
+    });
+  };
+  $scope.getRequest = function () {
+    console.log('getRequest begin');
+    $http({
+      method: 'GET',
+      url: serviceUrl + 'car/get',
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8'
+      }
+    }).then(function successCallback(success) {
+      console.log('getRequest success: ', success);
+      carGetRequest = success.data;
+      $scope.carGetResquest = carGetRequest;
+    }, function errorCallback(error) {
+      console.log('getRequest error: ', error);
+    }).finally(function () {
+      console.log('getRequest end');
+    });
+  };
+}]);
